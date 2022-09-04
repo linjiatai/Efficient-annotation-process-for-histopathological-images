9c2d6cbcffcd359040a09a40ea33907310edc59a
linjiatai <93721368+linjiatai@users.noreply.github.com>
2022-09-04 21:09:55 +0800

    Update 8_mask_filter.py

--- 8_mask_filter.py
+++ 8_mask_filter.py
@@ -3,6 +3,8 @@ from PIL import Image
 from skimage import morphology
 import cv2
 import numpy as np
+from palette import palette
+
 Image.MAX_IMAGE_PIXELS = None
 def mask_filter(mask):
     mask_np = np.asarray(mask)
@@ -29,16 +31,11 @@ def mask_filter(mask):
     
     return mask_np
 
-maskpath = '/media/linjiatai/linjiatai-16TB/兵兵/Multi-step/SAVE/Original_dataset/seg_mask/'
-maskpath_new = '/media/linjiatai/linjiatai-16TB/兵兵/Multi-step/SAVE/Original_dataset/seg_mask_filter/'
-palette = [0]*100
-palette[0:3] = [255,255,255]    # 白色 背景
-palette[3:6] = [120,120,120]    # 灰色 正常
-palette[6:9] = [255,0,0]        # 红色 肿瘤
-palette[9:12] = [0,255,0]       # 绿色 间质
-palette[12:15] = [0,255,255]    # 青色 粘液
-palette[15:18] = [255,0,255]    # 紫色 坏死
-palette[18:21] = [237,145,33]   # 土黄 肌肉
+maskpath = 'WSI/mask/'
+maskpath_new = 'WSI/seg_mask_filter/'
+if not os.path.exists(maskpath_new):
+    os.mkdir(maskpath_new)
+
 for root, _, files in os.walk(maskpath):
     for file in sorted(files):
         print(file)
