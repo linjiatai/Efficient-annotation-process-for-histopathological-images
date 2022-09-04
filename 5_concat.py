71350b827a5e9337dd8729e242e34f0e51f53a50
linjiatai <93721368+linjiatai@users.noreply.github.com>
2022-09-04 21:09:13 +0800

    Update 5_concat.py

--- 5_concat.py
+++ 5_concat.py
@@ -4,12 +4,18 @@ from PIL import Image
 dataroot = 'ROIs/'
 
 files = os.listdir(dataroot)
-tag = ['v0','v1','v2','v3']
-if not os.path.exists('stage2_multiple_concat/'):
-    os.mkdir('stage2_multiple_concat/')
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--version', type=str, default='v1')
+args = parser.parse_args()
+tag = args.version
+
 for file in sorted(files):
-    img = Image.new('RGB', (2000*3,2000*len(tag)))
-    for i in range(len(tag)):
-        tmp = Image.open('stage2_result_'+tag[i]+'/concat/'+file)
-        img.paste(tmp,(0,2000*i))
-    img.save('stage2_multiple_concat/'+file)
+    img = Image.open(dataroot+file).convert('RGB')
+    seg = Image.open('stage2_result_'+tag+'/seg/'+file).convert('RGB')
+    mask = Image.open('stage2_result_'+tag+'/mask/'+file).convert('RGB')
+    concat = Image.new('RGB', (2000*3,2000))
+    concat.paste(img,(0,0))
+    concat.paste(seg,(2000,0))
+    concat.paste(mask,(4000,0))
+    concat.save('stage2_result_'+tag+'/concat/'+file)
