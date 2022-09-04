e494b52b00a0163815d300eaa0fbd9ec174149a5
linjiatai <93721368+linjiatai@users.noreply.github.com>
2022-09-04 21:10:38 +0800

    Add files via upload

---
+++ palette.py
@@ -0,0 +1,10 @@
+## You can modify the palette to suit your dataset.
+
+palette = [0]*100
+palette[0:3] = [255,255,255]    # 白色 背景
+palette[3:6] = [120,120,120]    # 灰色 正常
+palette[6:9] = [255,0,0]        # 红色 肿瘤
+palette[9:12] = [0,255,0]       # 绿色 间质
+palette[12:15] = [0,255,255]    # 青色 粘液
+palette[15:18] = [255,0,255]    # 紫色 坏死
+palette[18:21] = [237,145,33]   # 土黄 肌肉
